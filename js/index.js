bbd9d155f816937045fc9b47109fde1e1d906b37
Aluno <renanandreolla@outlook.com>
2019-08-28 19:46:20 -0300

    Adição da conversão da temp. para firehinet

--- js/index.js
+++ js/index.js
@@ -5,24 +5,32 @@ function send(event) {
   var country = $("#country").val();
 
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+","
-  +country+"&appid=1136291543d398fff86ff109cf09ce1f", function(dados) {
+    +country+"&appid=1136291543d398fff86ff109cf09ce1f", function(dados) {
         console.log(dados.main.temp_max);
         console.log(dados.main.temp_min);
         console.log(dados.main.pressure);
         console.log(dados.main.humidity);
+        console.log(dados.cod);
 
         if ($("#celsius").prop("checked") === true) {
           $("#result").html("Hoje em "+city+", "+country+"</br>"+
           "Pressão atmosférica: "+ dados.main.pressure+"hpa"+"</br>"+
           "Umidade do ar: "+dados.main.humidity+"%"+"</br>"+
-          "Temperatura Máx: "+ Math.round(dados.main.temp_max - 273.15) +"</br>"+
-          "Temperatura Min: "+ Math.round(dados.main.temp_min - 273.15));
+          "Temperatura Máx: "+ Math.round(dados.main.temp_max - 273.15)+"°C"+"</br>"+
+          "Temperatura Min: "+ Math.round(dados.main.temp_min - 273.15)+"°C");
         }
+        if ($("#fahrenheit").prop("checked") === true) {
+          $("#result").html("Hoje em "+city+", "+country+"</br>"+
+          "Pressão atmosférica: "+ dados.main.pressure+"hpa"+"</br>"+
+          "Umidade do ar: "+dados.main.humidity+"%"+"</br>"+
+          "Temperatura Máx: "+ Math.round((dados.main.temp_max - 273.15)*(9/5)+32) +"°F"+"</br>"+
+          "Temperatura Min: "+ Math.round((dados.main.temp_min - 273.15)*(9/5)+32) +"°F");
+        }
+        //if (dados.cod == 404) {
+        //  $("#result").html("teste");
+        //}
     });
 };
-
-
-
 //Fahrenheit:
 //(0 K − 273,15) × 9/5 + 32 = -459,7 °F
 
